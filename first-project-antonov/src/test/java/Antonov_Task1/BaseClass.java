1e29c7b6ffde19e11437f1d1270d214abb4544cd
MeelooS <154681508+MeelooS@users.noreply.github.com>
2024-01-17 08:33:54 +0300

    Add files via upload

---
+++ first-project-antonov/src/test/java/Antonov_Task1/BaseClass.java
@@ -0,0 +1,32 @@
+package Antonov_Task1;
+
+import dev.failsafe.internal.util.Assert;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class BaseClass {
+    protected static WebDriver driver = new ChromeDriver();
+
+    @BeforeAll()
+
+
+
+    public static void s1(){
+        driver.get("http://localhost:8080/food");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        WebElement titleSpisokTovarov = driver.findElement(By.xpath("//div[@class='container-fluid']/h5"));
+        Assertions.assertEquals("Список товаров", titleSpisokTovarov.getText(),
+                "Список товаров не открылся");
+
+    }
+    @AfterAll()
+
+     public static void s2(){
+        driver.quit();
+     }
+}

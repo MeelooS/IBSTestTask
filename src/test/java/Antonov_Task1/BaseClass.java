bb2b512bcad67eec754532e813174b048c4c701b
MeelooS <154681508+MeelooS@users.noreply.github.com>
2024-01-20 20:51:11 +0300

    Add files via upload

---
+++ src/test/java/Antonov_Task1/BaseClass.java
@@ -0,0 +1,49 @@
+package Antonov_Task1;
+
+import dev.failsafe.internal.util.Assert;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.time.Duration;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+
+public class BaseClass {
+
+    public static WebDriver driver = new ChromeDriver() ;
+
+
+    @BeforeAll()
+    public static void openDriver()throws Exception {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("browserName", "chrome");
+        capabilities.setCapability("browserVersion", "108.0");
+        capabilities.setCapability("selenoid:option", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", false));
+        try {
+            driver = new RemoteWebDriver( new URI("http://149.154.71.152:4444/wd/hub").toURL(), capabilities);
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public  static void s1(){
+
+
+        driver.get("http://149.154.71.152:8080/food");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        WebElement titleSpisokTovarov = driver.findElement(By.xpath("//div[@class='container-fluid']/h5"));
+        Assertions.assertEquals("Список товаров", titleSpisokTovarov.getText(),
+                "Список товаров не открылся");
+    }
+
+}

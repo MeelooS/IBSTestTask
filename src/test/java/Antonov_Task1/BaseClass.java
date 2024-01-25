caa9584a7221f08c73262f1c46df9d45c6907bed
MeelooS <154681508+MeelooS@users.noreply.github.com>
2024-01-25 14:43:52 +0300

    Add files via upload

---
+++ src/test/java/Antonov_Task1/BaseClass.java
@@ -0,0 +1,49 @@
+package Antonov_Task1;
+
+import dev.failsafe.internal.util.Assert;
+import io.cucumber.java.Before;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import java.util.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.time.Duration;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+
+public class BaseClass {
+
+
+    public static WebDriver driver ;
+
+
+    public static void openDriver()throws Exception {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("browserName", "chrome");
+        capabilities.setCapability("browserVersion", "108.0");
+        capabilities.setCapability("selenoid:option", Map.<String, Object> of(
+                "enableVNC", true,
+                "enableVideo", false));
+        try {
+            driver = new RemoteWebDriver( new URI("http://149.154.71.152:4444/wd/hub").toURL(), capabilities);
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
+
+
+
---
+++ src/test/java/runner/TestRunner.java
@@ -0,0 +1,15 @@
+package runner;
+
+import org.junit.platform.suite.api.*;
+
+import static io.cucumber.core.options.Constants.*;
+
+@Suite
+@IncludeEngines("cucumber")
+@ConfigurationParameters({
+        @ConfigurationParameter(key = FEATURES_PROPERTY_NAME, value = "src/test/resources/features"),
+        @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "Steps"),
+        @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm")
+})
+public class TestRunner {
+}
